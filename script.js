89542d8c28a536dad0fde5d1829d02b90f87f56a
Taj036 <121137573+Taj036@users.noreply.github.com>
2023-12-07 13:12:44 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let firstInput = prompt("enter the input");
+let secInput = prompt("enter the input");
+let res1 = Number(firstInput) *2 ; 
+let res2 = Number(secInput);
+alert(`The final result is: ${res1+res2 }`);
